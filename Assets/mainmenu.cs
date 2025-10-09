703dafb25eac4aded9e80b88b0059736fba16cb3
Parth Sangale <parthsangale325@gmail.com>
2025-10-09 21:50:24 +0530

    MainMenu half done

---
+++ Assets/mainmenu.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+public class mainmenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Animator anim;
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    public void Load()
+    {
+        anim.SetBool("fade", true);
+        StartCoroutine(load());
+    }
+    IEnumerator load()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("SampleScene");
+    }
+}
